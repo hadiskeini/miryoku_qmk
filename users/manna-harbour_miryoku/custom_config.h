b6fb8a644782d2d1521696e73c9bc8f93506c25f
Hadi Skeini <hadiskeini@icloud.com>
2024-06-19 23:03:38 +0200

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -1,7 +1,56 @@
-// Copyright 2019 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 #pragma once
 
+// Define the keycodes for the combos
+enum combo_events {
+    COMBO_AE,
+    COMBO_OE,
+    COMBO_UE,
+    COMBO_LENGTH
+};
+
+uint16_t COMBO_LEN = COMBO_LENGTH;
+
+// Define the combo key arrays
+const uint16_t PROGMEM ae_combo[] = {KC_A, KC_E, COMBO_END};
+const uint16_t PROGMEM oe_combo[] = {KC_O, KC_E, COMBO_END};
+const uint16_t PROGMEM ue_combo[] = {KC_U, KC_E, COMBO_END};
+
+// Define the combo actions
+combo_t key_combos[COMBO_LENGTH] = {
+    [COMBO_AE] = COMBO(ae_combo, KC_QUOT),  // For ä
+    [COMBO_OE] = COMBO(oe_combo, KC_QUOT),  // For ö
+    [COMBO_UE] = COMBO(ue_combo, KC_QUOT)   // For ü
+};
+
+// Function to handle special characters
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case COMBO_AE:
+                tap_code16(KC_QUOT);
+                tap_code(KC_A);
+                return false;
+            case COMBO_OE:
+                tap_code16(KC_QUOT);
+                tap_code(KC_O);
+                return false;
+            case COMBO_UE:
+                tap_code16(KC_QUOT);
+                tap_code(KC_U);
+                return false;
+        }
+    }
+    return true;
+}
+
+// Override the COMBO_TERM if needed (optional)
+uint16_t get_combo_term(uint16_t index, combo_t *combo) {
+    switch(index) {
+        case COMBO_AE:
+        case COMBO_OE:
+        case COMBO_UE:
+            return 50; // Adjust this value as needed
+        default:
+            return COMBO_TERM;
+    }
+}
